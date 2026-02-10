197b67063d7e025b00f17f6c1fcc11ae5ce98aac
voliinyk-blackbird <voliinyk@blackbird-lab.com>
2026-02-10 21:09:27 +0400

    limit pins depends on zoom

--- src/modules/map/map.service.ts
+++ src/modules/map/map.service.ts
@@ -4,17 +4,14 @@ import { PrismaService } from "prisma/prisma.service";
 import { DB_EARTHQUAKE_NAME, SRID } from "../../constants";
 import { GetMapDto } from "./dto/get-map.dto";
 
-const MAX_LIMIT = 50_000;
+const MAX_LIMIT = 200_000;
 
 function getLimitByZoom(zoom?: number): number {
   if (zoom === undefined) return MAX_LIMIT;
 
-  if (zoom <= 4) return 5_000;
-  if (zoom <= 5) return 10_000;
-  if (zoom <= 6) return 15_000;
-  if (zoom <= 7) return 20_000;
-  if (zoom <= 8) return 30_000;
-  if (zoom <= 9) return 40_000;
+  if (zoom <= 4) return 50_000;
+  if (zoom <= 6) return 100_000;
+  if (zoom <= 8) return 150_000;
 
   return MAX_LIMIT;
 }
@@ -27,37 +24,48 @@ export class MapService {
     const { west, south, east, north, zoom } = dto;
     const limit = getLimitByZoom(zoom);
 
-    const data = await this.prisma.$queryRawUnsafe(
-      `
-      SELECT id, "externalId", "occuredAt", magnitude, depth, latitude, longitude, location, "createdAt"
-      FROM "${DB_EARTHQUAKE_NAME}"
-      WHERE geom && ST_MakeEnvelope($1, $2, $3, $4, $5)
-      LIMIT $6;
-    `,
-      west,
-      south,
-      east,
-      north,
-      SRID,
-      limit,
-    );
-
-    const total = await this.prisma.$queryRawUnsafe<[{ count: bigint }]>(
-      `
-      SELECT COUNT(*) as count
-      FROM "${DB_EARTHQUAKE_NAME}"
-      WHERE geom && ST_MakeEnvelope($1, $2, $3, $4, $5);
-    `,
-      west,
-      south,
-      east,
-      north,
-      SRID,
-    );
+    const [data, countResult] = await Promise.all([
+      this.prisma.$queryRawUnsafe<
+        {
+          id: number;
+          magnitude: number;
+          depth: number;
+          latitude: number;
+          longitude: number;
+          location: string | null;
+          occuredAt: Date;
+        }[]
+      >(
+        `
+        SELECT id, magnitude, depth, latitude, longitude, location, "occuredAt"
+        FROM "${DB_EARTHQUAKE_NAME}"
+        WHERE geom && ST_MakeEnvelope($1, $2, $3, $4, $5)
+        LIMIT $6;
+      `,
+        west,
+        south,
+        east,
+        north,
+        SRID,
+        limit,
+      ),
+      this.prisma.$queryRawUnsafe<[{ count: bigint }]>(
+        `
+        SELECT COUNT(*) as count
+        FROM "${DB_EARTHQUAKE_NAME}"
+        WHERE geom && ST_MakeEnvelope($1, $2, $3, $4, $5);
+      `,
+        west,
+        south,
+        east,
+        north,
+        SRID,
+      ),
+    ]);
 
     return {
       data,
-      total: Number(total[0].count),
+      total: Number(countResult[0].count),
       limit,
     };
   }
