efc87fd921c417778ee130c83cafbf1355d1f254
voliinyk-blackbird <voliinyk@blackbird-lab.com>
2026-02-10 16:46:57 +0400

    update cookies

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -14,7 +14,7 @@ export class AuthController {
 
     res.cookie("access_token", access_token, {
       httpOnly: true,
-      sameSite: "lax",
+      sameSite: "none",
       secure: process.env.NODE_ENV === "production",
     });
 
