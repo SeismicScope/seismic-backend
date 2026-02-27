e837fc09b01607684703e82a5dc71d2debfefe13
voliinyk-blackbird <voliinyk@blackbird-lab.com>
2026-02-28 01:48:21 +0400

    feat(map-points): increase zoom limit for level 4 and below from 50k to 70k

--- src/modules/map/helpers.ts
+++ src/modules/map/helpers.ts
@@ -3,7 +3,7 @@ const MAX_LIMIT = 200_000;
 export function getLimitByZoom(zoom?: number): number {
   if (zoom === undefined) return MAX_LIMIT;
 
-  if (zoom <= 4) return 50_000;
+  if (zoom <= 4) return 70_000;
   if (zoom <= 6) return 100_000;
   if (zoom <= 8) return 150_000;
 
